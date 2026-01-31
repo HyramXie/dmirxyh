9f74e345efe86174f938e1f5dd4903b83bb72e94
HyramXie <yuanhongxiedi@gmail.com>
2026-01-31 05:34:21 +0000

    complete train_llama_imagebind

--- train_llama_imagebind/script/run_base.sh
+++ train_llama_imagebind/script/run_base.sh
@@ -3,10 +3,10 @@ export CUDA_VISIBLE_DEVICES=2  # 指定使用哪张显卡，多卡用 "0,1"
 
 # --- 2. 路径配置 ---
 llm_path="/root/huggingface/llama/Meta-Llama-3-8B-Instruct"
-vision_path="/root/huggingface/google/siglip-so400m-patch14-384"
-data_path="/root/user/xyh/Datasets/MIntRec/MIntRec_train.json"
-output_dir="/root/user/xyh/train_llama/checkpoints/llama_mintrec_base"  # 每次实验可以改个名字
-num_frames=4
+vision_path="/root/huggingface/imagebind/imagebind_huge.pth"
+train_data_path="/root/user/xyh/Datasets/MIntRec2/MIntRec2_train.json"
+eval_data_path="/root/user/xyh/Datasets/MIntRec2/MIntRec2_dev.json"
+output_dir="/root/user/xyh/train_llama_imagebind/checkpoints/llama_mintrec2_base"  # 每次实验可以改个名字
 
 # --- 3. 训练超参数配置 ---
 batch_size=1          # 单卡 Batch Size (显存占用大建议为1)
@@ -15,6 +15,7 @@ epochs=3              # 训练轮数
 
 # --- 4. 日志与保存 ---
 logging_steps=10
+save_steps=500
 
 num_workers=0             # Dataloader 进程数 (显存吃紧或报错时设为0)
 
@@ -26,13 +27,14 @@ echo "Output: $output_dir"
 python ../train.py \
     --llm_path "$llm_path" \
     --vision_path "$vision_path" \
-    --data_path "$data_path" \
+    --train_data_path "$train_data_path" \
+    --eval_data_path "$eval_data_path" \
     --output_dir "$output_dir" \
-    --num_frames $num_frames \
     --batch_size $batch_size \
     --grad_accum_steps $grad_accum_steps \
     --epochs $epochs \
     --logging_steps $logging_steps \
+    --save_steps $save_steps \
     --num_workers $num_workers \
 
 echo "Training finished. Results saved to $output_dir"
